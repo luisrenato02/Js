acdf846aed3053bc62699f50c6a9ec6bad3efa1b
Luís Renato <89888509+luisrenato02@users.noreply.github.com>
2022-06-28 00:23:02 -0300

    indicador_periodo

---
+++ indicador_perido/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+var msg = window.document.getElementById('msg')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `São ${hora} Horas`
+
+if (hora >= 4 && hora <= 12){
+    bom.innerHTML = ("Bom dia!!")
+    document.body.style.background = 'yellow'
+    img.src="manha.png"
+
+} else if( hora > 12 && hora < 7){
+    bom.innerHTML = ("Boa tarde!!")
+    document.body.style.background = 'orange'
+    img.src="tarde.png"
+    
+} else {
+    bom.innerHTML = ("Boa noite!")
+    document.body.style.background = 'black'
+    img.src="noite.png"
+}
+}
